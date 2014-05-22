a36377015f1f779fd8381591ded339303e0aecec
maksimn <maksim1989sovunion@yandex.ru>
2014-05-22 04:04:09 +0400

    After a day of work

---
+++ FileSearcher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace FileSearchingWPF {
+    // FileSearcher class encapsulate logics to find files 
+    class FileSearcher {
+        private MainWindow mainWindow;
+        public Int32 NumFiles { set; get; } // Number of files processed
+        public String Directory { get; set; }
+        public String FilePattern { get; set; }
+
+        public void StartSearching() {
+            DirectoryInfo dir = new DirectoryInfo(Directory);
+            FindFiles(dir);
+        }
+
+        public void SetWindowToShowResults(MainWindow mainWindow) {
+            this.mainWindow = mainWindow;
+        }
+
+        private void FindFiles(DirectoryInfo dir) {
+            ProcessDirectories(dir);
+        }
+
+        private void ProcessDirectories(DirectoryInfo dir) {
+            try {
+                DirectoryInfo[] subdirs = dir.GetDirectories();
+                FileInfo[] files = dir.GetFiles();
+                foreach (var subdir in subdirs) {
+                    ProcessDirectories(subdir);
+                }
+                foreach (var file in files) {
+                    NumFiles++;
+                    mainWindow.qtyFilesLabel.Content = NumFiles;
+                    if (file.Name.Contains(FilePattern)) {
+                    }
+                }
+            } catch (Exception) {
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Forms;
 
 namespace FileSearchingWPF {
     public partial class MainWindow : Window {
+        // Fields:
+        private FolderBrowserDialog folderBrowserDialog;
+        private FileSearcher fileSearcher;
+
+        // Methods:
         public MainWindow() {
             InitializeComponent();
+            SetInitialValuesInTextBoxes();
+
+            fileSearcher = new FileSearcher();
         }
 
         private void MainWindowSizeChangedHandler(object sender, SizeChangedEventArgs e) {
@@ -12,6 +21,39 @@ namespace FileSearchingWPF {
                 treeView.Width = e.NewSize.Width - 250;
                 treeView.Height = e.NewSize.Height;
             }
+        }
+
+        private void MainWindowClosedHandler(object sender, EventArgs e) {
+            SaveSettingsOfTextBoxes();
+        }
+
+        private void SetInitialValuesInTextBoxes() {
+            folderTextBox.Text = Properties.Settings.Default.Folder;
+            fileTextBox.Text = Properties.Settings.Default.FileName;
+        }
+
+        private void SaveSettingsOfTextBoxes() {
+            Properties.Settings.Default.Folder = folderTextBox.Text;
+            Properties.Settings.Default.FileName = fileTextBox.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void folderButtonClickHandler(object sender, RoutedEventArgs e) {
+            this.folderBrowserDialog = new FolderBrowserDialog();
+            this.folderBrowserDialog.Description = "Задайте директорию для поиска файлов.";
+            this.folderBrowserDialog.SelectedPath = folderTextBox.Text;
+            DialogResult dialogResult = this.folderBrowserDialog.ShowDialog();
+            if (dialogResult == System.Windows.Forms.DialogResult.OK) {
+                folderTextBox.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void startButtonClickHandler(object sender, RoutedEventArgs e) {
+            treeView.Items.Clear();
+            fileSearcher.Directory = folderTextBox.Text;
+            fileSearcher.FilePattern = fileTextBox.Text;
+            fileSearcher.SetWindowToShowResults(this);
+            fileSearcher.StartSearching();
         }
     }
 }
